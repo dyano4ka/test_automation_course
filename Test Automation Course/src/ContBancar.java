c3631b583a63b86d9823d7fc1842c83ef686d106
Diana Ch <stamster.c@gmail.com>
2023-02-13 14:05:28 +0200

    ContBancar Created

---
+++ Test Automation Course/src/ContBancar.java
@@ -0,0 +1,23 @@
+public class ContBancar {
+    private String numarCont;
+    private String sold;
+
+    //Getter
+    public String getnumarCont(){
+        return numarCont;
+    }
+
+    public String getsold() {
+        return sold;
+    }
+
+    //Setter
+
+    public void setNumarCont(String newnumarCont) {
+        this.numarCont = newnumarCont;
+    }
+
+    public void setSold(String newsold) {
+        this.sold = newsold;
+    }
+}
