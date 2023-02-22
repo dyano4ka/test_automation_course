99b1841621a8659815fcc82455ae5e62afc7b144
Diana Ch <stamster.c@gmail.com>
2023-02-22 19:18:48 +0200

    Added Persoana class and edited Main

---
+++ Test Automation Course/topic2/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Persoana myPersoana = new Persoana();
+        myPersoana.setNume("Alex");
+        myPersoana.Prenume("Albu");
+        myPersoana.Virsta("34");
+        System.out.println(myPersoana.getNume() + " " + myPersoana.Prenume + " "+myPersoana.Virsta);
+    }
+}
